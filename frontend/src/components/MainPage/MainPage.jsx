c5d5a05ba2b3357ea74ad6ad5b66e604037a4bfe
sallylim <lim.sally20@gmail.com>
2022-01-15 15:49:29 -0800

    Created menu for main page

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainPage.module.css";
+import "./MainPage.css"
+import MenuCard from "./MenuCard/MenuCard";
 
 const MainPage = () => {
+  const [activeTab, setActiveTab] = useState("Leg")
+
   return (
-    <div className={styles.mainPage}>
-      <h1>MainPage</h1>
+    <div>
+      <div className={styles.mainPage}>
+        <h1>MainPage</h1>
+      </div>
+      <div className="menu">
+        <MenuCard workoutCategory="Leg" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MenuCard workoutCategory="Arm" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MenuCard workoutCategory="Chest" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MenuCard workoutCategory="Core" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MenuCard workoutCategory="Shoulder" activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MenuCard workoutCategory="Back" activeTab={activeTab} setActiveTab={setActiveTab} />
+      </div>
     </div>
+
+
   );
 };
 
