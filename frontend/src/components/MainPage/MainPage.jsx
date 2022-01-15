97d6920bfcfd11eae72ac47ff953f19ad21509c2
Amy Jo <amyjojjm2001@gmail.com>
2022-01-15 12:45:52 -0800

    react routing

---
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./MainPage.module.css";
+
+const MainPage = () => {
+  return (
+    <div className={styles.mainPage}>
+      <h1>MainPage</h1>
+    </div>
+  );
+};
+
+export default MainPage;
