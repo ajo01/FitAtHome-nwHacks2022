14febca9d75d6d110e25f20bad9aff9560d84df4
sallylim <lim.sally20@gmail.com>
2022-01-16 00:59:53 -0800

    disabled other buttons other than lateral raises

--- frontend/src/components/Workouts/Workout/Workout.jsx
+++ frontend/src/components/Workouts/Workout/Workout.jsx
@@ -5,9 +5,16 @@ const Workout = ({ name, type, image, key}) => {
   const navToLoadingPage = () => {
     window.location.href = "/loading";
   };
+  
+  const checkLateralRaise = () => {
+    if (name == "Lateral Raises"){
+      navToLoadingPage()
+    }
+  };
+
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imgDiv} onClick={navToLoadingPage}>
+      <div className={styles.imgDiv} onClick={checkLateralRaise}>
         <img className={styles.img} src={process.env.PUBLIC_URL + image} alt={name} />
       </div>
       <h2 className={styles.workoutName}>{name}</h2>
