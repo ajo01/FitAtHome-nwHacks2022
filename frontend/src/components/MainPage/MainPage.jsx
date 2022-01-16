e2aac96b1a35a0e853d12e95a6175b93060ca7c2
sallylim <lim.sally20@gmail.com>
2022-01-15 19:09:11 -0800

    fixed styling, need to add images

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import styles from "./MainPage.module.css";
 import Workouts from "../Workouts/Workouts";
 import SelectBodyPart from "../UI/SelectBodyPart/SelectBodyPart";
-
+import PTChar from "./img/PTChar.png"
 const MainPage = () => {
 
   return (
     <div className={styles.mainPage}>
-      <div className={styles.header}>
-        <h1>Exercise Selection</h1>
-        <h2>Choose a workout</h2>
+      <div className={styles.headerSection}>
+        <div className={styles.header}>
+          <h1 className={styles.mainHeader}>Exercise Selection</h1>
+          <h2 className={styles.subHeader}>Choose a workout</h2>
+        </div>
+          <img className={styles.icon} src={PTChar}/>
       </div>
-      <div style={{ height: "30px", width: "100%" }} />
-      <SelectBodyPart />
-      <Workouts className={styles.workoutSection} />
+      <div className={styles.mainContent}>
+        <SelectBodyPart />
+        <Workouts className={styles.workoutSection} />
+      </div>
     </div>
 
 
